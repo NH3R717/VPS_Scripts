8ddd1865f5aa99aeba223696cfb0c195ccc8690e
tommy <500@superscafe.com>
2021-06-10 12:15:09 -0400

    update – wagui_install ENV

--- web_wagui/wagui_install.sh
+++ web_wagui/wagui_install.sh
@@ -14,10 +14,13 @@ echo ${CONTAINER_DIR}
 ## Create dir for Docker container – set to user permissions
 mkdir -p ${CONTAINER_DIR} && cd "${CONTAINER_DIR}"
 
+# Add ENV for docker-compose.yml use
+echo "CONTAINER_DIR=${CONTAINER_DIR}" >> .env
+echo "DOMAIN_NAME=${DOMAIN_NAME}" >> .env
+
 ## Add website dir
 mkdir WebFiles && cd WebFiles
 # Add ENV for docker-compose.yml use
-echo "DOMAIN_NAME=${DOMAIN_NAME}" >> .env
 ## Import web files
 curl -LO https://github.com/NH3R717/Wagui-Restaurant/archive/refs/heads/master.zip
 ## uncompress webfiles and remove master.zip 
@@ -26,9 +29,6 @@ unzip master.zip && rm -rf master.zip
 cd Wagui-Restaurant-master && rm README.md .gitignore
 ## Copy files from Wagui-Restaurant-master to WebFiles
 cp -a . ../WebFiles && cd .. && rm -rf Wagui-Restaurant-master && cd ..
-# Add ENV for docker-compose.yml use
-echo "CONTAINER_DIR=${CONTAINER_DIR}" >> .env
-echo "DOMAIN_NAME=${DOMAIN_NAME}" >> .env
 # Import docker-compose.yml
 curl -L https://raw.githubusercontent.com/NH3R717/VPS_Scripts/master/web_wagui/docker-compose.yml > docker-compose.yml
 # Build and run container w/ ENV
