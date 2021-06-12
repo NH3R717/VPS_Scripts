b75b558c29558541dedcdf9d4c7e8345c9bc3dfe
tommy <500@superscafe.com>
2021-06-12 13:29:43 -0400

    test – nginx-proxy w/ ssl

--- nginx_proxi/nginx_proxy_install.sh
+++ nginx_proxi/nginx_proxy_install.sh
@@ -11,10 +11,15 @@ export CONTAINER_DIR="${HOME_DIR}/Docker/Nginx"
 
 echo ${CONTAINER_DIR}
 
-## Create dir for Docker container – set to user permissions
+# Create dir for Docker container – set to user permissions
 mkdir -p ${CONTAINER_DIR} && cd "${CONTAINER_DIR}"
+# For docker-gen
+mkdir -p /tmp/templates && cd /tmp/templates
+curl -o nginx.tmpl https://raw.githubusercontent.com/jwilder/docker-gen/master/templates/nginx.tmpl
+
 # Add ENV for docker-compose.yml use
 echo "VULTR_IP=${VULTR_IP}" >> .env
+echo "DEFAULT_EMAIL=${DEFAULT_EMAIL}" >> .env
 # Import docker-compose.yml
 curl -L https://raw.githubusercontent.com/NH3R717/VPS_Scripts/master/nginx_proxi/docker-compose.yml > docker-compose.yml
 # Create nginx network
