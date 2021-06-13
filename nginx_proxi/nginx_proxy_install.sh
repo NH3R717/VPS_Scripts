13925bd9dc00232edb88484426f61301f17a85ef
tommy <500@superscafe.com>
2021-06-13 14:21:10 -0400

    update – nginx proxy

--- nginx_proxi/nginx_proxy_install.sh
+++ nginx_proxi/nginx_proxy_install.sh
@@ -8,7 +8,6 @@ set -euxo pipefail
 ###########################
 
 export CONTAINER_DIR="${HOME_DIR}/Docker/Nginx"
-
 echo ${CONTAINER_DIR}
 
 # For docker-gen
@@ -19,12 +18,19 @@ curl -o nginx.tmpl https://raw.githubusercontent.com/jwilder/docker-gen/master/t
 mkdir -p ${CONTAINER_DIR} && cd "${CONTAINER_DIR}"
 
 # Add ENV for docker-compose.yml use
-echo "VULTR_IP=${VULTR_IP}" >> .env
-echo "DEFAULT_EMAIL=${DEFAULT_EMAIL}" >> .env
+# echo "VULTR_IP=${VULTR_IP}" >> .env
+echo NGINX_WEB=nginx-web >> .env
+echo DOCKER_GEN=nginx-gen >> .env
+echo LETS_ENCRYPT=nginx-letsencrypt >> .env
+echo NETWORK=webproxy >> .env
+echo NGINX_FILES_PATH=${CONTAINER_DIR} >> .env
+echo SERVER_PATH=$(pwd)/.server >> .env
+echo ROOT_PATH=$(pwd) >> .env
+
 # Import docker-compose.yml
 curl -L https://raw.githubusercontent.com/NH3R717/VPS_Scripts/master/nginx_proxi/docker-compose.yml > docker-compose.yml
 # Create nginx network
-sudo docker network create nginx-proxy
+sudo docker network create ${NETWORK}
 # Build and run container
 docker-compose up -d --build
 
