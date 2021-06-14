a566799fc45779199f57a365c745c78e7f7f7c4e
tommy <500@superscafe.com>
2021-06-14 16:08:32 -0400

    update  – wagui un comment html volume

--- nginx_proxi/nginx_proxy_install.sh
+++ nginx_proxi/nginx_proxy_install.sh
@@ -21,22 +21,22 @@ mkdir -p ${CONTAINER_DIR} && cd "${CONTAINER_DIR}"
 
 # Add ENV for docker-compose.yml use
 # echo "VULTR_IP=${VULTR_IP}" >> .env
-echo NGINX_WEB=web_wagui >> .env
-echo DOCKER_GEN=nginx-gen >> .env
-echo LETS_ENCRYPT=nginx-letsencrypt >> .env
-echo NETWORK=webproxy >> .env
-echo IP=0.0.0.0 >> .env
-echo NGINX_FILES_PATH=${CONTAINER_DIR} >> .env
-echo SERVER_PATH=$(pwd)/.server >> .env 
-echo ROOT_PATH=$(pwd) >> .env
+# echo NGINX_WEB=web_wagui >> .env
+# echo DOCKER_GEN=nginx-gen >> .env
+# echo LETS_ENCRYPT=nginx-letsencrypt >> .env
+# echo NETWORK=webproxy >> .env
+# echo IP=0.0.0.0 >> .env
+# echo NGINX_FILES_PATH=${CONTAINER_DIR} >> .env
+# echo SERVER_PATH=$(pwd)/.server >> .env 
+# echo ROOT_PATH=$(pwd) >> .env
 echo CONTAINER_DIR="${HOME_DIR}/Docker/Nginx" >> .env
 echo "LETSENCRYPT_EMAIL=${DEFAULT_EMAIL}"
 
 
 # 4. Download the latest version of nginx.tmpl
 
-mkdir -p /etc/docker-gen/templates/
-curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > /etc/docker-gen/templates/nginx.tmpl
+# mkdir -p /etc/docker-gen/templates/
+# curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > /etc/docker-gen/templates/nginx.tmpl
 
 
 # Import docker-compose.yml
