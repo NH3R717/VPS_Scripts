6a6be5fb16a260846dd05f9a05e4b585f6fff8d3
tommy <500@superscafe.com>
2021-06-15 13:49:18 -0400

    update – network

--- nginx_proxi/nginx_proxy_install.sh
+++ nginx_proxi/nginx_proxy_install.sh
@@ -42,7 +42,7 @@ echo "LETSENCRYPT_EMAIL=${DEFAULT_EMAIL}"
 # Import docker-compose.yml
 curl -L https://raw.githubusercontent.com/NH3R717/VPS_Scripts/master/nginx_proxi/docker-compose.yml > docker-compose.yml
 # Create nginx network
-#! sudo docker network create ${NETWORK}
+sudo docker network create ${NETWORK}
 # Build and run container
 docker-compose up -d --build
 
