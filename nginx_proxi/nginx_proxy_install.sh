737b083a7c2d9f332aef0e5728cf9dc38b9d217f
tommy <500@superscafe.com>
2021-06-23 11:07:36 -0400

    refactor – comment conventions & echo

--- nginx_proxi/nginx_proxy_install.sh
+++ nginx_proxi/nginx_proxy_install.sh
@@ -7,17 +7,17 @@ set -euxo pipefail
 ### Nginx Install & Run ###
 ###########################
 
-export CONTAINER_DIR="${HOME_DIR}/Docker/Nginx"
+CONTAINER_DIR="${HOME_DIR}/Docker/Nginx"
 echo "Created container dir ENV ${CONTAINER_DIR}"
 
-# Create dir for Docker container 
+# create dir for Docker container & change dir
 mkdir -p ${CONTAINER_DIR} && cd "${CONTAINER_DIR}"
 
-# Import docker-compose.yml
+# import docker-compose.yml
 curl -L https://raw.githubusercontent.com/NH3R717/VPS_Scripts/master/nginx_proxi/docker-compose.yml > docker-compose.yml
-# Create nginx network
+# create nginx network
 sudo docker network create ${NETWORK}
-# Build and run container
+# build and run container
 docker-compose up -d --build
 
 #  set to user permissions
