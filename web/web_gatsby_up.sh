c9ae30d261c7c2256e00608b6c4933c8d3e715f3
tommy <500@superscafe.com>
2021-07-22 13:11:10 -0400

    !update – web gatsby up script (local build)

--- web/web_gatsby_up.sh
+++ web/web_gatsby_up.sh
@@ -34,15 +34,9 @@ echo "DEFAULT_EMAIL=${DEFAULT_EMAIL}" >> .env
 ## set RAM memory swap to HD (@ <05%)
 echo vm.swappiness=05 | sudo tee -a /etc/sysctl.conf
 ## import web files
-# curl -LO https://github.com/NH3R717/${PROJECT_NAME}/archive/refs/heads/${GIT_BRANCH}.zip
 mkdir site
 curl -L https://raw.githubusercontent.com/NH3R717/${PROJECT_NAME}/${GIT_BRANCH}/docker-compose.yml > docker-compose.yml
 curl -L https://raw.githubusercontent.com/NH3R717/${PROJECT_NAME}/${GIT_BRANCH}/site/index.html > site/index.html
-## uncompress webfiles and remove master.zip 
-# unzip ${GIT_BRANCH}.zip && rm -rf ${GIT_BRANCH}.zip
-# mv .env "${PROJECT_NAME}-${GIT_BRANCH}"
-## build and run container w/ ENV
-# cd "${PROJECT_NAME}-${GIT_BRANCH}"
 echo \
 '
 
@@ -53,12 +47,7 @@ echo \
 '
 echo "– ${PWD}"
 sudo docker-compose up -d --build
-
-## move docker files & remove build files
-# mv docker-compose.yml Dockerfile app .env ..
-# cd ..
-# rm -rf "${PROJECT_NAME}-${GIT_BRANCH}"    
-
+   
 ## set to user permissions
 sudo chmod 0750 "${CONTAINER_DIR}"
 sudo chown --recursive \
