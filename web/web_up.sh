dbecc0aede4ff621ff014aaeab2ed6f7ea9cff42
tommy <17020768+NH3R717@users.noreply.github.com>
2022-06-08 21:28:32 -0400

    refactor/add – wagui to nginx up

--- web/web_up.sh
+++ web/web_up.sh
@@ -2,29 +2,31 @@
 echo \
 '
 
-#################################
-### Install Script Pt.2 Start ###
-#################################
+#######################
+### Install Scriptt ###
+#######################
 
 '
+export DOCKER_WEB=~/Docker/Web
+mkdir ${DOCKER_WEB}
+echo "DOCKER_WEB=${DOCKER_WEB}" >> .env
 
-# add ENV for docker-compose.yml use
+## add ENV for docker-compose.yml use (Supers)
+export DOCKER_WEB_SUPERS=~/Docker/Web/Supers
+mkdir -pv ${DOCKER_WEB_SUPERS}
+echo "DOCKER_WEB_SUPERS=${DOCKER_WEB_SUPERS}" >> .env
+## add ENV for docker-compose.yml use (Wagui)
+export DOCKER_WEB_WAGUI=~/Docker/Web/Wagui
+mkdir -pv ${DOCKER_WEB_WAGUI}
+echo "DOCKER_WEB_WAGUI=${DOCKER_WEB_WAGUI}" >> .env
 
-echo "CONTAINER_DIR=${CONTAINER_DIR}" >> .env
+
+## add ENV for docker-compose.yml use
 export NETWORK
 echo "NETWORK=${NETWORK}" >> .env
 
-export PROJECT_NAME
-echo "PROJECT_NAME=${PROJECT_NAME}" >> .env
-export GIT_BRANCH
-echo "GIT_BRANCH=${GIT_BRANCH}" >> .env
-
 export DOMAIN_NAME_1
 echo "DOMAIN_NAME_1=${DOMAIN_NAME_1}" >> .env
-export DOMAIN_NAME_2
-echo "DOMAIN_NAME_2=${DOMAIN_NAME_2}" >> .env
-export DOMAIN_NAME_3
-echo "DOMAIN_NAME_3=${DOMAIN_NAME_3}" >> .env
 export DOMAIN_NAME_4
 echo "DOMAIN_NAME_4=${DOMAIN_NAME_4}" >> .env
 export DOMAIN_NAME_5
@@ -32,33 +34,48 @@ echo "DOMAIN_NAME_5=${DOMAIN_NAME_5}" >> .env
 export DEFAULT_EMAIL
 echo "DEFAULT_EMAIL=${DEFAULT_EMAIL}" >> .env
 
-## add website dir
-mkdir html && cd html
-## import web files
-curl -LO https://github.com/NH3R717/${PROJECT_NAME}/archive/refs/heads/${GIT_BRANCH}.zip
-## uncompress webfiles and remove master.zip 
-unzip ${GIT_BRANCH}.zip && rm -rf ${GIT_BRANCH}.zip
-## remove unnecessary files
-cd "${PROJECT_NAME}-${GIT_BRANCH}" && sudo rm -rf sc && sudo rm -f README.md .gitignore LICENSE gatsby* package* ${PROJECT_NAME}_install.sh
-## copy files from *-branch to htlm
-cp -a . .. && cd .. && sudo rm -rf "${PROJECT_NAME}-${GIT_BRANCH}" && cd ..
-echo "Current working directory ${PWD}"
+# projects
+cd ~/Docker/Web && mkdir Supers/Home Supers/Blog Wagui
 
-# import docker-compose.yml
-sudo curl -L https://raw.githubusercontent.com/NH3R717/${PROJECT_NAME}/${GIT_BRANCH}/docker-compose.yml > docker-compose.yml
-# build and run container w/ ENV
+## set RAM memory swap to HD (swap @ <05%)
+echo vm.swappiness=05 | sudo tee -a /etc/sysctl.conf
+##! run docker compose (Supers)
+cd /Supers
+curl -L https://raw.githubusercontent.com/NH3R717/VPS_Scripts/master/web/supers/docker-compose.yml > docker-compose.yml
+## import default web html
+curl -L https://raw.githubusercontent.com/NH3R717/VPS_Scripts/master/web/supers/default_page/index.html | tee \
+${DOCKER_WEB_SUPERS}/Home/index.html \
+${DOCKER_WEB_SUPERS}/Blog/index.html
 sudo docker-compose up -d --build
 
-# set to user permissions
-sudo chmod 0750 "${CONTAINER_DIR}"
+##! run docker compose (Wagui)
+cd ../Wagui
+curl -L https://raw.githubusercontent.com/NH3R717/VPS_Scripts/master/web/wagui/docker-compose.yml > docker-compose.yml
+## import default web html
+curl -L https://raw.githubusercontent.com/NH3R717/VPS_Scripts/master/web/wagui/default_page/index.html \
+${DOCKER_WEB_WAGUI}/index.html \
+sudo docker-compose up -d --build
+   
+## set to user permissions
+sudo chmod 0750 "${DOCKER_WEB}"
 sudo chown --recursive \
-"${USERNAME}":"${USERNAME}" "${CONTAINER_DIR}"
+"${USERNAME}":"${USERNAME}" "${DOCKER_WEB}"
 
 echo \
 '
 
-####################################
-### Install Script Pt.2 Complete ###
-####################################
+###############################
+### Install Script Complete ###
+###############################
 
 '
+
+## curl -L https://raw.githubusercontent.com/NH3R717/VPS_Scripts/master/web/web_supers_up.sh | exec bash
+
+## cd ~/Docker/Nginx && sudo docker-compose logs -f
+
+## cd ~/Docker/Web/Supers && sudo docker-compose logs -f
+
+## /Volumes/CLT/web_current/Vultr/Web/web_supers/NavHub/public
+
+## cd ~/Docker/Web/Supers && sudo docker-compose down && sudo docker container prune -f && sudo docker image prune -f && cd ~/Docker && sudo rm -rf ~/Docker/Web
