1936f8e5fa8c32c479817d65b8ff28ebf0de7d14
NH3R717 <500@superscafe.com>
2022-07-06 18:09:09 -0400

    Update and rename redacted_boot_script_example.sh to starter_boot_script.sh

--- example_boot_script/starter_boot_script.sh
+++ example_boot_script/starter_boot_script.sh
@@ -1,9 +1,9 @@
-###! Contained is a redacted sample version of this project's Linux VPS boot script used to create the Docker hosted NGINX reverse proxy server and Wireguard VPN
-
-
 #!/bin/bash
 # Run Fail Safe Command
 #set -euxo pipefail
+
+
+###! Contained is the example version of this project's Linux VPS boot script used to create the Docker hosted NGINX reverse proxy server and Wireguard VPN
 
 
 ##! Add Vultr Start-Up Scripts Through Web Interface
@@ -11,7 +11,7 @@
 
 # *********************************************
 # Automated Secure Base Ubuntu / Debian Install
-# Developer: supers cafe/NH3R717
+# Developer: S Cafe/NH3R717
 # *********************************************
 
 
@@ -36,7 +36,7 @@ echo HOME_DIR=${HOME_DIR} >> "/etc/environment"
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:${HOME_DIR}/bin
 echo PATH=${PATH} >> "/etc/environment"
 
-export WIREGUARD_PORT=46840
+export WIREGUARD_PORT=46555
 echo WIREGUARD_PORT=${WIREGUARD_PORT} >> "/etc/environment"
 
 export VULTR_IP=00.00.000.000
@@ -71,7 +71,6 @@ echo DEFAULT_EMAIL=${DEFAULT_EMAIL} >> "/etc/environment"
 useradd --create-home --shell "/bin/bash" \
 --groups sudo "${USERNAME}"
 
-### Remove sudo password entry requirement
 echo \
 '
 ###########################
@@ -79,13 +78,14 @@ echo \
 ###########################
 
 ' \
+# Remove sudo password entry requirement
 "${USERNAME}" ALL=\(ALL\) NOPASSWD:ALL \
 >> /etc/sudoers
 
 # Create SSH directory for sudo user
 mkdir "${HOME_DIR}/.ssh"
 
-# Public key for the new sudo user
+# Copy public key for the new sudo user
 cp /root/.ssh/authorized_keys "${HOME_DIR}/.ssh"
 
 # Adjust sudo user SSH configuration ownership and permissions
@@ -112,7 +112,7 @@ sed --in-place 's/#\?\(PasswordAuthentication\s*\).*$/\1no/' \
 sed --in-place 's/#\?\(X11Forwarding\s*\).*$/\1no/' \
 /etc/ssh/sshd_config
 
-sed --in-place 's/#\?\(Port\s*\).*$/\55555/' \
+sed --in-place 's/#\?\(Port\s*\).*$/\34555/' \
 /etc/ssh/sshd_config
 
 # Restart SSH Daemon
@@ -127,7 +127,7 @@ systemctl restart sshd
 # /etc/default/ufw
 
 # Create new SSH port w/ limit (Default 6 connect attempts within 30 sec)
-ufw limit 55555/tcp
+ufw limit 34555/tcp
 # Open standard web ports
 ufw allow 80/tcp
 ufw allow 443/tcp
